bdd18a7aa55f8abe726a198e4a4f35205ae66d2c
giacomobranchi <branchigiacomo28@gmail.com>
2023-12-05 17:23:37 +0100

    add dish model, migration and seeder

---
+++ database/migrations/2023_12_05_150706_create_dishes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dishes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->text('ingredients');
+            $table->decimal('price', 6, 2);
+            $table->boolean('available');
+            $table->string('img')->nullable();
+            $table->string('slug');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dishes');
+    }
+};
---
+++ database/seeders/DishSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Dish;
+use Illuminate\Support\Str;
+
+
+use function Pest\Laravel\json;
+
+
+
+class DishSeeder extends Seeder
+{
+
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        $ingredients = [
+            'Rum',
+            'Lime juice',
+            'Sugar',
+            'Mint',
+            'Soda',
+        ];
+        for ($i = 0; $i < 10; $i++) {
+            $dish = new Dish();
+            $dish->name = Str::random(10);
+            $dish->description = Str::random(30);
+            $dish->ingredients = implode(', ', $ingredients);
+            $dish->price = 10;
+            $dish->available = true;
+            $dish->img = Str::random(10);
+            $dish->slug = Str::slug($dish->name, '-');
+            $dish->save();
+        }
+    }
+}
