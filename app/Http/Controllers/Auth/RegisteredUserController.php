77b7d8d7bdd857c992caca96cadfcc68c2499291
PietroCipolla95 <pietro.cipolla.95@gmail.com>
2023-12-07 16:43:30 +0100

    fix old value for radios

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -39,7 +39,7 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
-            'p_iva' => ['required', 'string', 'min:16', 'max:16'],
+            'p_iva' => ['required', 'string', 'min:11', 'max:11'],
             'address' => ['required', 'string', 'max:255'],
             'types' => ['required']
         ]);
