f83f7f371a457d05ffd6481b89754534fe0db356
lorenzo-neri <lorenzo21neri@gmail.com>
2023-12-17 23:28:25 +0100

    add table in orders.index with data

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -6,20 +6,59 @@
             Your Orders
         </h2>
 
-        @forelse($ordersData as $data)
-            <div>
-                {{ $data->dish_id }}
-            </div>
-            <div>
-                {{ $data->total_price }} €
-            </div>
-            <div>
-                {{ $data->name }}
-            </div>
-        @empty
-            No more orders
-        @endforelse
+        <div class="table-responsive my-4">
+            <table class="table border table-striped table-hover table-light">
+                <thead>
+                    <tr>
+                        <th>Order ID</th>
+                        <th>Order Price</th>
+                        <th>Info UI</th>
+                        <th>Dish ID</th>
+                        <th>Quantity</th>
+                    </tr>
+                </thead>
+                <tbody>
 
+                    @forelse($ordersData as $data)
+                        <tr>
+                            <td>
+                                {{ $data->order_id }}
+                            </td>
+                            <td>
+                                {{ $data->total_price }}
+                            </td>
+                            <td>
+                                <div>
+                                    {{ $data->ui_name }}
+                                </div>
+                                <div>
+                                    {{ $data->ui_address }}
+                                </div>
+                                <div>
+                                    {{ $data->ui_mail }}
+                                </div>
+                                <div>
+                                    {{ $data->ui_phone }}
+                                </div>
+                            </td>
+                            <td>
+                                {{ $data->dish_id }}
+                            </td>
+                            <td>
+                                {{ $data->qty }}
+                            </td>
+                        </tr>
+                    @empty
+                        <h3 class="py-3">
+                            No orders at the moment
+                        </h3>
+                    @endforelse
+
+
+
+                </tbody>
+            </table>
+        </div>
 
     </div>
 @endsection
